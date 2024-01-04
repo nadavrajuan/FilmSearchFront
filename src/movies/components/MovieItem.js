8d65608b629228e3062890dc30e7b65543fcd500
NataliaSh012 <shnatalya1989@gmail.com>
2024-01-04 19:55:32 +0200

    apply random transform forMovieItem

--- src/movies/components/MovieItem.js
+++ src/movies/components/MovieItem.js
@@ -1,15 +1,62 @@
-import './movieItem.css'
+
+// const MovieItem = ({ title, overview }) => {
+//   return (
+//     <div className="card-container" >
+//       <div className="card">
+//         <div className="front-content" style={{border: '3px solid green'}}>
+//           <p>{title}</p>
+//         </div>
+//         <div className="content" style={{border: '3px solid violet'}}>
+//           <p className="heading">{title}</p>
+//           <p>
+//            {overview.length> 200 ? overview.substring(0, 200) + '...' : overview}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import './movieItem.css';
+
 const MovieItem = ({ title, overview }) => {
+  const [transformVariant, setTransformVariant] = useState('');
+
+  useEffect(() => {
+    const variants = ['1', '2', '3', '4'];
+    const randomVariant = variants[Math.floor(Math.random() * variants.length)];
+    setTransformVariant(randomVariant);
+    console.log(randomVariant);
+  }, []);
+
+  const contentClass = classNames({
+    'card-content-transform-1': transformVariant === '1',
+    'card-content-transform-2': transformVariant === '2',
+    'card-content-transform-3': transformVariant === '3',
+    'card-content-transform-4': transformVariant === '4',
+
+  });
+
+  const frontContentClass = classNames({
+    'card-front-content-transform-1': transformVariant === '1',
+    'card-front-content-transform-2': transformVariant === '2',
+    'card-front-content-transform-3': transformVariant === '3',
+    'card-front-content-transform-4': transformVariant === '4',
+
+  });
+
   return (
     <div className="card-container" >
       <div className="card">
-        <div className="front-content">
+        <div className={`front-content ${frontContentClass}`} >
           <p>{title}</p>
         </div>
-        <div className="content">
+        <div className={`content ${contentClass}`} >
           <p className="heading">{title}</p>
           <p>
-           {overview.length> 200 ? overview.substring(0, 200) + '...' : overview}
+            {overview.length > 200 ? overview.substring(0, 200) + '...' : overview}
           </p>
         </div>
       </div>
@@ -17,4 +64,4 @@ const MovieItem = ({ title, overview }) => {
   );
 };
 
-export default MovieItem
+export default MovieItem;
